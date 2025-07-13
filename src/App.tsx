91b8ef2f3a688610abd99be86732908eabba4aaf
rhoeunbin <nhodq219@gmail.com>
2025-07-13 17:01:36 +0900

    add : 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BoardItem from "components/BoardItem";
+import { latestBoardListMock, top3boardListMock } from "mocks";
+import Top3Item from "components/Top3Item";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            {/* {latestBoardListMock.map((boardListItem) => (
+                <BoardItem boardListItem={boardListItem} />
+            ))} */}
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    gap: "24px",
+                }}
+            >
+                {top3boardListMock.map((top3ListItem) => (
+                    <Top3Item top3ListItem={top3ListItem} />
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default App;
