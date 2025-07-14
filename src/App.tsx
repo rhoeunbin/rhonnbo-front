8c98b3d949778ab6338910b39b0d40feb487bacd
rhoeunbin <nhodq219@gmail.com>
2025-07-14 23:14:31 +0900

    ✨add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,12 @@ import BoardItem from "components/BoardItem";
 import { latestBoardListMock, top3boardListMock } from "mocks";
 import Top3Item from "components/Top3Item";
 import InputBox from "components/InputBox";
+import Footer from "layouts/footer";
 
 function App() {
-    const [value, setValue] = useState<string>("");
     return (
         <>
-            {/* {latestBoardListMock.map((boardListItem) => (
-                <BoardItem boardListItem={boardListItem} />
-            ))} */}
-            {/* <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: "24px",
-                }}
-            >
-                {top3boardListMock.map((top3ListItem) => (
-                    <Top3Item top3ListItem={top3ListItem} />
-                ))}
-            </div> */}
-            <InputBox
-                label="이메일"
-                type="text"
-                placeholder="이메일 주소를 입력해주세요"
-                value={value}
-                error={true}
-                setValue={setValue}
-                message="babo"
-            />
+            <Footer />
         </>
     );
 }
