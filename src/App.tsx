6cf6f4b8fc088c7d88c09ccf0be73c80b57073f6
rhoeunbin <nhodq219@gmail.com>
2025-07-14 17:09:18 +0900

    ✨add inputBox components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BoardItem from "components/BoardItem";
 import { latestBoardListMock, top3boardListMock } from "mocks";
 import Top3Item from "components/Top3Item";
+import InputBox from "components/InputBox";
 
 function App() {
+    const [value, setValue] = useState<string>("");
     return (
         <>
             {/* {latestBoardListMock.map((boardListItem) => (
                 <BoardItem boardListItem={boardListItem} />
             ))} */}
-            <div
+            {/* <div
                 style={{
                     display: "flex",
                     justifyContent: "center",
@@ -20,7 +22,16 @@ function App() {
                 {top3boardListMock.map((top3ListItem) => (
                     <Top3Item top3ListItem={top3ListItem} />
                 ))}
-            </div>
+            </div> */}
+            <InputBox
+                label="이메일"
+                type="text"
+                placeholder="이메일 주소를 입력해주세요"
+                value={value}
+                error={true}
+                setValue={setValue}
+                message="babo"
+            />
         </>
     );
 }
