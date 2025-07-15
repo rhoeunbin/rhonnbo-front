b73a0cd292c09cd6aae9c26309372b1a46654e92
rhoeunbin <rhoeb21@gmail.com>
2025-07-15 22:05:17 +0900

    ✨add Header setting - mypage,login,logout

--- src/types/interface/index.ts
+++ src/types/interface/index.ts
@@ -1,5 +1,6 @@
+import User from "./user.interface";
 import BoardListItem from "./board-list-item.interface";
 import FavoriteListItem from "./favorite-list-item.interface";
 import CommentListItem from "./commnt-list-item.interface";
 
-export type { BoardListItem, FavoriteListItem, CommentListItem };
+export type { User, BoardListItem, FavoriteListItem, CommentListItem };
