7231712bb1470a30587e405f9ea3e8e0031bb914
rhoeunbin <rhoeb21@gmail.com>
2025-08-06 23:01:55 +0900

    ✨feat : add board detail page event handler

---
+++ src/mocks/board.mock.ts
@@ -0,0 +1,19 @@
+import { Board } from "types/interface";
+
+const boardMock: Board = {
+    boardNumber: 1,
+    title: "강아지는 정말 기여오",
+    content:
+        "강아지는 정말 기여운 거 같어 어떻게 이렇게 기여운 생명체가 있으까 정말 기여워 강아지는 정말 기여운 거 같어 어떻게 이렇게 기여운 생명체가 있으까 정말 기여워 강아지는 정말 기여운 거 같어 어떻게 이렇게 기여운 생명체가 있으까 정말 기여워 강아지는 정말 기여운 거 같어 어떻게 이렇게 기여운 생명체가 있으까 정말 기여워",
+    boardImageList: [
+        "https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/06/30/22/34/dog-5357794_1280.jpg",
+    ],
+    writeDatetime: "2025.08.18. 11:12:27",
+    writerEmail: "eun@email.com",
+    writerNickname: "논빈",
+    writerProfileImage:
+        "https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_1280.jpg",
+};
+
+export default boardMock;
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -2,8 +2,10 @@ import latestBoardListMock from "mocks/latest-board-list.mock";
 import top3boardListMock from "./top3-board-list.mock";
 import commentListMock from "./comment-list.mock";
 import favoriteListMock from "./favorite-list.mock";
+import boardMock from "./board.mock";
 
 export {
+    boardMock,
     commentListMock,
     favoriteListMock,
     latestBoardListMock,
