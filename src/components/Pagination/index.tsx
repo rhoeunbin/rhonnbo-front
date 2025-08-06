bf5d5ea00b29ea6d6b72afd7aa1880f89c61d8bf
rhoeunbin <rhoeb21@gmail.com>
2025-08-06 17:42:42 +0900

    🎨 Style: Refine board detail top layout and styles

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+//      component : 페이지네이션 컴포넌트       //
+export default function Pagination() {
+    //      render : 페이지네이션 컴포넌트 렌더링       //
+    return <div>pagination</div>;
+}
