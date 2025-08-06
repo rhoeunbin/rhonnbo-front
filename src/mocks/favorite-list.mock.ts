1c617a50af03d5c4448879fd725214153ec5c556
rhoeunbin <rhoeb21@gmail.com>
2025-08-06 15:41:16 +0900

    ✨feat : add favorite list component

---
+++ src/mocks/favorite-list.mock.ts
@@ -0,0 +1,48 @@
+const favoriteListMock = [
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+    {
+        email: "eun@gmail.com",
+        nickname: "논빈이",
+        profileImage: null,
+    },
+];
+export default favoriteListMock;
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,4 +1,11 @@
 import latestBoardListMock from "mocks/latest-board-list.mock";
 import top3boardListMock from "./top3-board-list.mock";
+import commentListMock from "./comment-list.mock";
+import favoriteListMock from "./favorite-list.mock";
 
-export { latestBoardListMock, top3boardListMock };
+export {
+    commentListMock,
+    favoriteListMock,
+    latestBoardListMock,
+    top3boardListMock,
+};
