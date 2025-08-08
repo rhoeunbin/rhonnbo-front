eb5323bc86c4aeb5a2ee39894df827ecb6c6db7f
rhoeunbin <rhoeb21@gmail.com>
2025-08-08 23:01:38 +0900

    ✨feat : Add board detail comment - pagination

--- src/views/Board/Detail/index.tsx
+++ src/views/Board/Detail/index.tsx
@@ -36,6 +36,7 @@ import {
 import dayjs from "dayjs";
 import { useCookies } from "react-cookie";
 import { PostCommentRequestDto } from "apis/request/board";
+import { usePagination } from "hooks";
 
 //      component: 게시물 상세 화면 컴포넌트        //
 export default function BoardDetail() {
@@ -229,20 +230,32 @@ export default function BoardDetail() {
     const BoardDetailBottom = () => {
         //      state : 댓글 textarea 참조 상태     //
         const commentRef = useRef<HTMLTextAreaElement | null>(null);
+
+        //      state : 페이지네이션 관련 상태      //
+        const {
+            currentPage,
+            currentSection,
+            viewList,
+            viewPageList,
+            totalSection,
+            setCurrentPage,
+            setCurrentSection,
+            setTotalList,
+        } = usePagination<CommentListItem>(3);
         //      state : 좋아요 리스트 상태      //
         const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>(
             []
         );
-        //      state : 댓글 리스트 상태      //
-        const [commentList, setCommentList] = useState<CommentListItem[]>([]);
         //      state : 좋아요 상태      //
         const [isFavorite, setFavorite] = useState<boolean>(false);
         //      state : 좋아요 리스트 보기 상태      //
         const [showFavorite, setShowFavorite] = useState<boolean>(false);
+        //      state : 댓글 상태      //
+        const [comment, setComment] = useState<string>("");
+        //      state : 전체 댓글 개수 상태      //
+        const [totalCommentCount, setTotalCommentCount] = useState<number>(0);
         //      state : 댓글 리스트 보기 상태      //
         const [showComment, setShowComment] = useState<boolean>(false);
-        //      state : 댓글 리스트 보기 상태      //
-        const [comment, setComment] = useState<string>("");
 
         //      function : get favorite list response 처리 함수     //
         const getFavoriteListResponse = (
@@ -276,7 +289,8 @@ export default function BoardDetail() {
             if (code !== "SU") return;
 
             const { commentList } = responseBody as GetCommentListResponseDto;
-            setCommentList(commentList);
+            setTotalList(commentList);
+            setTotalCommentCount(commentList.length);
         };
 
         //      function : put favorite response 처리 함수      //
@@ -412,7 +426,7 @@ export default function BoardDetail() {
                         <div className="icon-button">
                             <div className="icon comment-icon"></div>
                         </div>
-                        <div className="board-detail-buttom-button-text">{`댓글 ${commentList.length}`}</div>
+                        <div className="board-detail-buttom-button-text">{`댓글 ${totalCommentCount}`}</div>
                         <div
                             className="icon-button"
                             onClick={onShowCommentClickHandler}
@@ -448,18 +462,25 @@ export default function BoardDetail() {
                             <div className="board-detail-bottom-comment-title">
                                 {"댓글 "}
                                 <span className="emphasis">
-                                    {commentList.length}
+                                    {totalCommentCount}
                                 </span>
                             </div>
                             <div className="board-detail-bottom-comment-list-container">
-                                {commentList.map((item) => (
+                                {viewList.map((item) => (
                                     <CommentItem commentListItem={item} />
                                 ))}
                             </div>
                         </div>
                         <div className="divider"></div>
                         <div className="board-detail-bottom-comment-pagination-box">
-                            <Pagination />
+                            <Pagination
+                                currentPage={currentPage}
+                                currentSection={currentSection}
+                                setCurrentPage={setCurrentPage}
+                                setCurrentSection={setCurrentSection}
+                                viewPageList={viewPageList}
+                                totalSection={totalSection}
+                            />
                         </div>
                         {loginUser !== null && (
                             <div className="board-detail-bottom-comment-input-box">
